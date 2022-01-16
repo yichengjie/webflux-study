93a21c06fe906f4cb5768085df283f09540bb03f
易成杰 <626659321@qq.com>
2022-01-16 21:17:48 +0800

    save

--- hello-r2dbc/src/main/java/com/yicj/r2dbc/controller/UserInfoController.java
+++ hello-r2dbc/src/main/java/com/yicj/r2dbc/controller/UserInfoController.java
@@ -2,11 +2,13 @@ package com.yicj.r2dbc.controller;
 
 import com.yicj.r2dbc.entity.UserInfo;
 import com.yicj.r2dbc.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+@Slf4j
 @RestController
 public class UserInfoController {
 
@@ -15,6 +17,7 @@ public class UserInfoController {
 
     @GetMapping("/user/findAll")
     public Flux<UserInfo> findAll(){
+        log.info("==> controller findAll ....");
         return userService.findAll();
     }
 
