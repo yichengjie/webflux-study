957b37dd8fd1617c3baf32597dd6e1d2639405f4
易成杰 <626659321@qq.com>
2022-01-30 09:41:48 +0800

    sav

--- hello-reactor/src/test/java/com/yicj/reactor/hello/SimpleFluxTest.java
+++ hello-reactor/src/test/java/com/yicj/reactor/hello/SimpleFluxTest.java
@@ -6,6 +6,8 @@ import reactor.core.publisher.Flux;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 public class SimpleFluxTest {
@@ -21,8 +23,55 @@ public class SimpleFluxTest {
     }
 
     @Test
-    public void test02(){
-        List<Flux<Integer>> fluxes = Arrays.asList(Flux.just(1), Flux.just(2), Flux.just(3));
+    public void rollback() throws ExecutionException, InterruptedException {
+        CompletableFuture<Object> cf = CompletableFuture.supplyAsync(() -> {
+            throw new RuntimeException("异常报错");
+        })/*.exceptionally(throwable -> {
+            log.info("error: {}", throwable.getMessage());
+            throw new RuntimeException(throwable.getMessage());
+        })*/;
+        // 回滚测试
+        CompletableFuture<String> futureA = CompletableFuture.supplyAsync(() -> {
+            log.info("启动A");
+            try {
+                Thread.sleep(1000);
+                // do busi
+            } catch (InterruptedException e) {
+                // 异常处理
+                e.printStackTrace();
+            }
+            return "completableFutureA";
+        }).exceptionally(throwable -> {
+            log.error("error : {}", throwable.getMessage());
+            throw new RuntimeException(throwable.getMessage());
+        }).thenCombine(cf, (s1, s2) -> s1 + " , " + s2);
 
+        CompletableFuture<String> futureB = CompletableFuture.supplyAsync(() -> {
+            log.info("启动B");
+            try {
+                Thread.sleep(100);
+                // do busi
+            } catch (InterruptedException e) {
+                // 异常处理
+                e.printStackTrace();
+            }
+            return "completableFutureB";
+        });
+
+        CompletableFuture<String> futureC = futureA.thenApplyAsync(c -> {
+            log.info("启动C");
+            try {
+                // do busi
+            } catch (Exception e) {
+                // 异常处理
+                e.printStackTrace();
+            }
+            return "completableFutureC";
+        });
+
+        //
+        CompletableFuture<Void> future = CompletableFuture.allOf(futureA, futureB, futureC);
+        log.info("resultA: {}, resultB: {}, resultC: {}", futureA.get(), futureB.get(), futureC.get());
+        log.info("result : {}", future.get());
     }
 }
