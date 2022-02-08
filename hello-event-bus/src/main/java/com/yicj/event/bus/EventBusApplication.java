fe0ed50cf80b946fdb9d92fb038d170562cfce9f
yichengjie <626659321@qq.com>
2022-02-08 14:08:54 +0800

    save

---
+++ hello-event-bus/src/main/java/com/yicj/event/bus/EventBusApplication.java
@@ -0,0 +1,18 @@
+package com.yicj.event.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: webflux-study
+ * @description:
+ * @author: yicj1
+ * @create: 2022-02-08 13:58
+ **/
+@SpringBootApplication
+public class EventBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventBusApplication.class, args) ;
+    }
+}
---
+++ hello-event-bus/src/main/java/com/yicj/event/bus/config/EventConfig.java
@@ -0,0 +1,16 @@
+package com.yicj.event.bus.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+/**
+ * @program: webflux-study
+ * @description:
+ * @author: yicj1
+ * @create: 2022-02-08 13:59
+ **/
+@Configuration
+public class EventConfig {
+
+}
