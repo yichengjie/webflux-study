1e49c5c173dcb25790f212e652ee80e606ccc525
易成杰 <626659321@qq.com>
2022-03-26 22:34:26 +0800

    save

---
+++ docs/webflux学习笔记.md
@@ -0,0 +1,2 @@
+1. spring默认的异步执行器为SimpleAsyncTaskExecutor
+2. 
--- hello-world/src/main/java/com/yicj/hello/config/AppConfig.java
+++ hello-world/src/main/java/com/yicj/hello/config/AppConfig.java
@@ -2,8 +2,8 @@ package com.yicj.hello.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
@@ -14,7 +14,14 @@ public class AppConfig {
     @Bean("asyncExecutor")
     public Executor asyncExecutor(){
         // 默认异步执行器
-        return new SimpleAsyncTaskExecutor() ;
+        //return new SimpleAsyncTaskExecutor() ;
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor() ;
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(3);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("AsyncMvc-");
+        executor.initialize();
+        return executor ;
     }
 
 }
